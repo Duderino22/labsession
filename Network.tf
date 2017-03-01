4eb2e5fdd8a24157918470927dac1d3dc70e59ac
Duderino22 <sebastien.ringuet@polymtl.ca>
2017-02-28 19:19:34 -0500

    changes applied labsession
    changes applied terraform labsession

--- Network.tf
+++ Network.tf
@@ -3,19 +3,25 @@ resource "google_compute_network" "cr460labsession" {
   auto_create_subnetworks = "false"
 }
 
-resource "google_compute_subnetwork" "Public" {
-  name          = "Public"
-  ip_cidr_range = "172.31.0.0/24"
+resource "google_compute_subnetwork" "public" {
+  name          = "public"
+  ip_cidr_range = "172.16.1.0/24"
+  network       = "${google_compute_network.cr460labsession.self_link}"
+  region        = "us-west1"
+}
+
+resource "google_compute_subnetwork" "workload" {
+  name          = "workload"
+  ip_cidr_range = "10.1.1.0/24"
   network       = "${google_compute_network.cr460labsession.self_link}"
   region        = "us-east1"
 }
 
-
-resource "google_compute_subnetwork" "Workload" {
-  name          = "Workload"
-  ip_cidr_range = "10.0.1.0/24"
+resource "google_compute_subnetwork" "backend" {
+  name          = "backend"
+  ip_cidr_range = "192.168.0.0/24"
   network       = "${google_compute_network.cr460labsession.self_link}"
-  region        = "us-centra1"
+  region        = "us-central1"
 }
 
 resource "google_compute_firewall" "web" {
@@ -29,16 +35,10 @@ resource "google_compute_firewall" "web" {
 
 resource "google_compute_firewall" "ssh" {
   name    = "ssh"
-  network = "${google_compute_network.cr460.name}"
+  network = "${google_compute_network.cr460labsession.name}"
   allow {
     protocol = "tcp"
     ports    = ["22"]
   }
 
-  resource "google_compute_firewall" "https" {
-    name    = "https"
-    network = "${google_compute_network.cr460.name}"
-    allow {
-      protocol = "tcp"
-      ports    = ["443"]
-    }
+}
