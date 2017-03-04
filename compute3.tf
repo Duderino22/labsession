c863cbb1d63cd38e5efa8a3d092b3d6ba0e81e19
Duderino22 <sebastien.ringuet@polymtl.ca>
2017-03-04 15:06:41 -0500

    compute file 3 added
    compute file 3 added
    VMs etcd 1 etcd 2 etcd 3 created

---
+++ compute3.tf
@@ -0,0 +1,41 @@
+  machine_type = "f1-micro"
+  zone         = "us-east1-b"
+
+  disk {
+    image = "coreos-cloud/coreos-stable"
+  }
+
+  network_interface {
+    access_config {
+
+    }
+  }
+}
+
+  machine_type = "f1-micro"
+  zone         = "us-east1-b"
+
+  disk {
+    image = "coreos-cloud/coreos-stable"
+  }
+
+  network_interface {
+    access_config {
+
+    }
+  }
+}
+
+  machine_type = "f1-micro"
+  zone         = "us-east1-b"
+
+  disk {
+    image = "coreos-cloud/coreos-stable"
+  }
+
+  network_interface {
+    access_config {
+
+    }
+  }
+}
